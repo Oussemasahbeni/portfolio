a0bf0143d01615d9bd2a9464a8cc460f85131a1d
oussema sahbeni <oussemasahbeni300@gmail.com>
2025-11-23 20:38:48 +0100

    feat: update language component; replace HTML template with inline template and add language icons

--- src/app/layout/language/language.component.ts
+++ src/app/layout/language/language.component.ts
@@ -5,6 +5,9 @@ import {
   signal,
 } from '@angular/core';
 import { TranslocoService } from '@jsverse/transloco';
+import { provideIcons } from '@ng-icons/core';
+import { lucideLanguages } from '@ng-icons/lucide';
+import { radixCheck } from '@ng-icons/radix-icons';
 import { BrnMenuTrigger } from '@spartan-ng/brain/menu';
 import { HlmButtonImports } from '@spartan-ng/helm/button';
 import { HlmIconImports } from '@spartan-ng/helm/icon';
@@ -18,19 +21,61 @@ import { HlmMenu, HlmMenuItem } from '@spartan-ng/helm/menu';
     HlmMenu,
     HlmMenuItem,
     HlmIconImports,
+    HlmIconImports,
   ],
-  templateUrl: './language.component.html',
+  template: `
+    <button
+      hlmBtn
+      [brnMenuTriggerFor]="menu"
+      title="Change language"
+      size="icon"
+      variant="outline"
+      class="size-8"
+    >
+      <ng-icon name="lucideLanguages" hlm size="sm" />
+    </button>
+
+    <ng-template #menu>
+      <hlm-menu class="min-w-32">
+        <button
+          hlmMenuItem
+          (click)="setLanguage('en')"
+          [class.font-semibold]="activeLang() === 'en'"
+        >
+          <span>English</span>
+
+          @if(activeLang() === 'en') {
+          <ng-icon name="radixCheck" hlm />
+          }
+        </button>
+        <button
+          hlmMenuItem
+          (click)="setLanguage('fr')"
+          [class.font-semibold]="activeLang() === 'fr'"
+        >
+          <span>Français</span>
+          @if(activeLang() === 'fr') {
+          <ng-icon name="radixCheck" hlm />
+          }
+        </button>
+      </hlm-menu>
+    </ng-template>
+  `,
+  providers: [
+    provideIcons({
+      lucideLanguages,
+      radixCheck,
+    }),
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LanguageComponent {
-  public translocoService = inject(TranslocoService);
+  private translocoService = inject(TranslocoService);
 
-  public activeLang = signal(
-    this.translocoService.getActiveLang().toUpperCase()
-  );
+  readonly activeLang = signal(this.translocoService.getActiveLang());
 
   setLanguage(lang: string): void {
     this.translocoService.setActiveLang(lang);
-    this.activeLang.set(lang.toUpperCase());
+    this.activeLang.set(lang);
   }
 }
